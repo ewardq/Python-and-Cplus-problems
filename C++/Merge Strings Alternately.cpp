0d1149da3d9f02b2fe504e9eb1f97199358fee45
Eduardo Martínez Piña <72580785+ewardq@users.noreply.github.com>
2024-12-15 23:58:37 -0600

    Add new solution

---
+++ C++/Merge Strings Alternately.cpp
@@ -0,0 +1,36 @@
+/* 
+ - - - - - - - - - - - - - - - - - - - - T A S K  - - - - - - - - - - - - - - - - - - - -
+ - - - - - - - - - - - - - - - - - - - - T A S K  - - - - - - - - - - - - - - - - - - - -
+*/
+
+#include <iostream>
+ 
+using namespace std;
+
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int m = word1.size();
+        int n = word2.size();
+        string result = "";
+        int i = 0, j = 0;
+
+        while (i < m || j < n) {
+            if (i < m) {
+                result.push_back(word1[i++]);
+            }
+            if (j < n) {
+                result.push_back(word2[j++]);
+            }
+        }
+
+        return result;
+    }
+};
+
+
+/* 
+====================================  TOP SOLUTION   ====================================
+
+=========================================================================================  
+*/
